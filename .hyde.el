ec84e607a67a2f9e9287c65fd48d7c02b7b112c3
Noufal Ibrahim <noufal@nibrahim.net.in>
2013-09-03 16:10:45 +0530

    Change image directory

--- .hyde.el
+++ .hyde.el
@@ -1,4 +1,4 @@
-(setq hyde/deploy-command "rsync -vr _site/* noufal@noufalibrahim.name:/home/noufal/www/thelycaeum.in/blog"
+(setq hyde/deploy-command "rsync -vr _site/* noufal@noufalibrahim.name:/home/noufal/www/thelycaeum.in/"
       hyde/jekyll-command "jekyll build"
       hyde-images-dir "img")
 
