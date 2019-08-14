d4dbdc800327415e6ec275739a87212cddbf470d
Noufal Ibrahim <noufal@hamon.in>
2019-08-14 12:09:50 +0530

    Updates deployment URL

--- .hyde.el
+++ .hyde.el
@@ -1,4 +1,4 @@
-(setq hyde/deploy-command "rsync -vr _site/* noufal@noufalibrahim.name:/home/noufal/www/thelycaeum.in/"
+(setq hyde/deploy-command "rsync -vr _site/* thelycaeum.in:/var/www/html/thelycaeum.in"
       hyde/jekyll-command "jekyll build"
       hyde-images-dir "img")
 
